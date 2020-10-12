b1febe7d3468eae3c943c8964319f239dae4cae0
--阿木--木-- <11@.com>
2020-10-12 16:22:42 +0800

    jjj

---
+++ app.js
@@ -0,0 +1,143 @@
+const koa = require('koa');
+const StaticCache = require('koa-static-cache');
+const Router = require('koa-router');
+const BodyParser = require('koa-bodyparser');
+const fs = require('fs');
+
+/**
+ * 通过服务器请求拿到一个基础页面，后续的内容就不要再通过浏览器发请求获取了
+ * 因为通过浏览器发请求就会导致浏览器重新渲染，跳转，新开窗口
+ * 
+ * 首先通过浏览器拿到一个基础页面
+ * 然后在基础页面中写入js，通过js的ajax来发送请求，ajax发送请求并不会直接渲染页面
+ * 而是会把获取到的数据存储在ajax对象下
+ */
+
+let datas = JSON.parse(fs.readFileSync('./data/data.json'));
+
+const app = new koa();
+
+// 静态
+app.use( StaticCache('./static', {
+    prefix: '/static',
+    gzip: true
+}) );
+
+// body 解析
+app.use( BodyParser() );
+
+const router = new Router();
+
+router.get('/', async ctx => {
+    ctx.body = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>-</p>
+    </body>
+    </html>`;
+});
+
+// let datas1 = JSON.parse(fs.readFileSync('./static/js.js'));
+// router.get('/j', async ctx => {
+//     ctx.body = datas1;
+// });
+
+router.get('/todos', async ctx => {
+    // ctx.body = {x:1, y: 2}; //{"x":1, "y:2"}
+
+    // ctx.body = datas.todos;
+
+    ctx.body = {
+        code: 0,
+        data: datas.todos
+    }
+});
+
+router.post('/toggle', async ctx => {
+    let id = ctx.request.body.id || 0;
+
+    if (!id) {
+        ctx.body = {
+            code: 1,
+            data: '请传入id'
+        }
+        return;
+    }
+
+    let todo = datas.todos.find( todo => todo.id == id );
+    todo.done = !todo.done;
+
+    ctx.body = {
+        code: 0,
+        data: todo
+    }
+
+    fs.writeFileSync('./data/data.json', JSON.stringify(datas));
+});
+
+router.post('/remove', async ctx => {
+    let id = ctx.request.body.id || 0;
+
+    if (!id) {
+        ctx.body = {
+            code: 1,
+            data: '请传入id'
+        }
+        return;
+    }
+
+    datas.todos = datas.todos.filter(todo => todo.id != id);
+
+    // let todo = datas.todos.find( todo => todo.id == id );
+
+    ctx.body = {
+        code: 0,
+        data: '删除成功'
+    }
+
+    fs.writeFileSync('./data/data.json', JSON.stringify(datas));
+});
+
+// router.post('/add', async ctx => {
+router.get('/add', async ctx => {
+
+    let data = ctx.request.query || {};
+
+    // let title = ctx.request.body.title || '';
+    // if (!title) {
+    //     ctx.body = {
+    //         code: 1,
+    //         data: '请传入任务标题'
+    //     }
+    //     return;
+    // }
+
+    // let newTask = {
+    //     id: ++datas._id,
+    //     title,
+    //     done: false
+    // };
+
+    // datas.todos.push(newTask);
+    // ctx.body = {
+    //     code: 0,
+    //     data: newTask
+    // }
+
+    datas.todos.push(data);
+    ctx.body = {
+        code: 0,
+        data: ''
+    }
+
+    fs.writeFileSync('./data/data.json', JSON.stringify(datas));
+});
+
+app.use( router.routes() );
+
+app.listen(80);
---
+++ static/js.js
@@ -0,0 +1,19 @@
+//群类型
+export const GroupType = [
+    { value: '0', label: '福利', type: 'normal', selfBuilt: true, comp: 'Fuli' },
+    { value: '1', label: '扫雷', type: 'bomb', selfBuilt: true, comp: 'Saolei' },
+    { value: '2', label: '牛牛', type: 'niuniu', selfBuilt: true, comp: 'Niuniu' },
+    { value: '3', label: '禁抢', type: 'jinqiang', selfBuilt: false, comp: 'Jinqiang' },
+    { value: '4', label: '抢庄牛牛', type: 'robniuniu', selfBuilt: true, comp: 'Robniu' },
+    { value: '5', label: '二八杠', type: 'erbagang', selfBuilt: true, comp: 'Robniu' },
+    { value: '6', label: '龙虎斗', type: 'longhd', selfBuilt: false, comp: 'Longhd' },
+    { value: '7', label: '接龙', type: 'jielong', selfBuilt: false, comp: 'Jielong' },
+    { value: '8', label: '二人牛牛', type: 'twoniuniu', selfBuilt: true, comp: 'Niuniu' },
+    { value: '9', label: '超级扫雷', type: 'superBomb', selfBuilt: false, comp: 'SuperBomb' },
+    { value: '10', label: '包包彩', type: 'baoBaoCai', selfBuilt: false, comp: 'baoBaoCai' },
+    { value: '11', label: '包包牛', type: 'NiuNiuBao', selfBuilt: false, comp: 'niuNiuBao' },
+    // { value: '12', label: '百人牛牛', type: 'NiuNiuBao', selfBuilt: false, comp: 'niuNiuBao' },
+    // { value: '13', label: '极速扫雷', type: 'NiuNiuBao', selfBuilt: false, comp: 'niuNiuBao' },
+    { value: '14', label: '百人牛牛', type: 'bairenniuniu', selfBuilt: false, comp: 'bairenniuniu' },
+    { value: '15', label: '极速扫雷', type: 'jisusaolei', selfBuilt: false, comp: 'jisusaolei' },
+  ];
