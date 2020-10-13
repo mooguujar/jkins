b0ddcc376d116ab4a235a28492d13310dfc4f06b
--阿木--木-- <11@.com>
2020-10-13 16:41:01 +0800

    43

--- app.js
+++ app.js
@@ -158,43 +158,18 @@ router.post('/reset', async ctx => {
     var logss='fa';
     console.log(current_time+'重新发布12');
     
-    child.exec('ls', function(err, sto) {
-        // console.log(sto);//sto才是真正的输出，要不要打印到控制台，由你自己啊
-        logss+=sto;console.log(logss);
-    });
     
-    child.exec('cd /data/jkins', function(err, sto) {
-        if (err) {
-            console.log(`exec error: ${err}`);
-            ctx.body = {code: 0,data: '失败'};
-            return;
-        }
-        logss+=sto;console.log(logss);
+    child.exec('cd /data/jkins', function(err, sto,tr) {
+        logss+=sto;console.log(logss+'--'+tr+'===');
 
-        child.exec('git pull', function(err, sto) {
-            if (err) {
-                console.log(`exec error: ${err}`);
-                ctx.body = {code: 0,data: '失败'};
-                return;
-            }
-            logss+=sto;console.log(logss);
+        child.exec('git pull', function(err, sto,tr) {
+            logss+=sto;console.log(logss+'--'+tr+'===');
 
-            child.exec('npm i', function(err, sto) {
-                if (err) {
-                    console.log(`exec error: ${err}`);
-                    ctx.body = {code: 0,data: '失败'};
-                    return;
-                }
-                logss+=sto;console.log(logss);
+            child.exec('npm i', function(err, sto,tr) {
+                logss+=sto;console.log(logss+'--'+tr+'===');
 
                 child.exec('pm2 restart app', function(err, sto,tr) {
-                    console.log(tr);
-                    if (err) {
-                        console.log(`exec error: ${err}`);
-                        ctx.body = {code: 0,data: '失败'};
-                        return;
-                    }
-                    logss+=sto;console.log(logss);
+                    logss+=sto;console.log(logss+'--'+tr+'===');
                 });
             });
         });
@@ -216,5 +191,5 @@ process.on('uncaughtException', function (err) {
     console.log('Caught exception: ' + err);
 })
 
-console.log(current_time+'--启动成功---!!测试1122335444');
+console.log(current_time+'--启动成功---!!测试 666');
 
