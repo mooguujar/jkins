237277d21a828350933a4cf02cb161148f2cf384
Your Name <you@example.com>
2020-10-23 21:59:08 +0800

    444

--- app.js
+++ app.js
@@ -61,6 +61,19 @@ app.use( BodyParser() );
 
 const router = new Router();
 
+app.use(async (ctx, next)=>{
+    try{
+        await next();   // 执行后代的代码
+        if(!ctx.body){  // 没有资源
+            ctx.body = "404"
+        }
+    }catch(e){
+        // 如果后面的代码报错 返回500
+        ctx.body = "500"
+    }
+})
+
+
 router.get('/', async ctx => {
     // ctx.body = ``; 
     ctx.response.redirect('http://cryptosjsorg.cf');
