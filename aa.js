ae44c4dc3b17725b94ae3852c3ed4ec8d0beb20f
Your Name <you@example.com>
2020-10-23 15:24:35 +0800

    5435

--- aa.js
+++ aa.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer'); //引入模块
 let transporter = nodemailer.createTransport({
     //node_modules/nodemailer/lib/well-known/services.json  查看相关的配置，如果使用qq邮箱，就查看qq邮箱的相关配置
     service: 'qq', //类型qq邮箱
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    // port: 465,
+    // secure: true, // true for 465, false for other ports
     auth: {
         user: '1162212711@qq.com', // 发送方的邮箱
         pass: 'zgcrkvlhymguhahg' // smtp 的授权码
