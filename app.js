2009c16ea0c9347aaea2b6d5c5dd079768fa40f7
amu <yy@gmail.com>
2023-06-15 01:52:59 +0800

    232

--- app.js
+++ app.js
@@ -460,7 +460,7 @@ const options = {
 // app.listen(80);
 
 https.createServer(options
-    ,  app.callback()).listen(3001, (err) => {  
+    ,  app.callback()).listen(443, (err) => {  
         if (err) {
         console.log('server error: ', err);
         } else {
