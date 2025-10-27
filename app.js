10ba9ed51352c0317ee264d9a77d500613cb3f78
amu <yy@gmail.com>
2025-10-27 22:35:48 +0700

    22

--- app.js
+++ app.js
@@ -140,8 +140,8 @@ app.use(async (ctx, next) => {
         if (!ctx.body) {  // 没有资源
             var str = ctx.href || ctx.request.header.host; //请求地址
             var str1 = ctx.header.referer || ctx.request.header.host;//输入栏
-            // console.log(str);
-            // console.log('str1',str1);
+            console.log(str);
+            console.log('str1',str1);
 
             // console.log('11访问', str1, str);//gdpayweb.fyi
 
